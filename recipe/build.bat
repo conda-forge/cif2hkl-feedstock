5464156bae3da5d45716e0ca4070800ddac8f72e
Peter Willendrup <pkwi@fysik.dtu.dk>
2024-11-24 14:51:08 +0100

    Attempt a windows build

---
+++ recipe/build.bat
@@ -0,0 +1,13 @@
+mkdir build
+cd build
+
+cmake ^
+    -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+    ../src ^
+    -DCMAKE_INSTALL_LIBDIR=lib ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -G "NMake Makefiles" ^
+    %CMAKE_ARGS%
+
+cmake --build . --config Release
+cmake --build . --target install --config Release
